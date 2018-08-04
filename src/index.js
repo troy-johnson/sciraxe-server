59c28ec7e07a9a5f2c21d74ce3d089ecee0b7486
Troy Johnson <troy.johnson57@gmail.com>
2018-08-04 11:17:59 -0600

    Dockerized project

--- src/index.js
+++ src/index.js
@@ -1,64 +1,39 @@
-const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "test"
-  },
-  {
-    id: "link-1",
-    url: "www.troy-johnson.com",
-    description: "its-a-me"
-  }
-];
-
-let idCount = links.length;
+// const { GraphQLServer } = require("graphql-yoga");
+import { GraphQLServer } from 'graphql-yoga';
+import { Prisma } from 'prisma-binding';
+// const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
     info: () => `Sciraxe API`,
-    feed: () => links,
-    link: args => {
-      for (let i; i < links.length; i++) {
-        console.log(links[i]);
-        if (links[i].id === args.id) {
-          return links[i];
-        } 
-      }
-    }
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
     },
-    delete: (root, args) => {
-      for (let i; i < links.length; i++) {
-        if (links[i].id === args.id) {
-          links.splice(i, 1);
-        }
-      }
-      return links;
-    },
-    put: (root, args) => {
-      for (let i; i < links.length; i++) {
-        if (links[i].id === id) {
-          (links[i].url = args.url), (links[i].description = args.description);
-        }
-      }
-    }
-  }
-};
+  },
+}
 
 const server = new GraphQLServer({
   typeDefs: "src/schema.graphql",
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/troy-johnson-4e96b5/sciraxe-database/dev',
+      secret: 'greenIce4',
+      debug: true
+    })
+  })
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
